a48b3e672f89f8f85598b5fbc66e4691a6b25332
Mikhail <Mikhail.Kirsanov@cern.ch>
2022-05-04 11:04:08 +0200

    Fix bug in the DMBrem process

--- src/DMG4/DMProcesses/DMProcessDMBrem.cc
+++ src/DMG4/DMProcesses/DMProcessDMBrem.cc
@@ -91,9 +91,8 @@ G4VParticleChange* DMProcessDMBrem::PostStepDoIt( const G4Track& aTrack,
   }
   G4double DME = incidentE * XAcc;
   G4double DMM = myDarkMatter->GetMA()*GeV;
-  G4double DMKinE = DME*DME - DMM*DMM;
+  G4double DMKinE = DME - DMM;
   if(DMKinE < 0.) DMKinE = 0.;
-  DMKinE = sqrt(DMKinE);
 
   // Initialize DM direction vector:
   G4ThreeVector DMDirection(0., 0., .1);
