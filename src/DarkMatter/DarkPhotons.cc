5eb5c434fff44748eef658d1fa09fa71e5b15c5c
Mikhail <Mikhail.Kirsanov@cern.ch>
2023-10-03 16:05:16 +0200

    Bug fixed

--- src/DarkMatter/DarkPhotons.cc
+++ src/DarkMatter/DarkPhotons.cc
@@ -116,7 +116,11 @@ double DarkPhotons::TotalCrossSectionCalc(double E0)
 
 double DarkPhotons::GetSigmaTot(double E0)
 {
-  return GetSigmaTot0(E0);
+  if(MA > 0.001) {
+    return GetSigmaTot0(E0);
+  } else {
+    return TotCSVectorParticle(MA);
+  }
 }
 
 
