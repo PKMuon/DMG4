58c2079ac9a6794b913dc204ba20ad43a02d3c94
Andrea Celentano <andrea.celentano@ge.infn.it>
2023-10-07 15:30:12 +0200

    fix typo

--- src/DarkMatter/DarkMassSpin2Annihilation.cc
+++ src/DarkMatter/DarkMassSpin2Annihilation.cc
@@ -108,6 +108,6 @@ void DarkMassSpin2Annihilation::SetMA(double MAIn) {
     mChi2 = mChi;
 }
 
-double DarkMassSpin2Annihilation::AngularDistributionResonant(doube E0,double eta){
+double DarkMassSpin2Annihilation::AngularDistributionResonant(double E0,double eta){
   return 1.;
 }
