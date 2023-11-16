ec0fdcd49b54163a9cc390948cbfbd72a0a81ba4
Andrea Celentano <andrea.celentano@ge.infn.it>
2023-11-16 09:11:40 +0100

    Fixing a wrong factor *Z -atomic number- in the DarkZAnnihilation

--- src/DarkMatter/DarkZAnnihilation.cc
+++ src/DarkMatter/DarkZAnnihilation.cc
@@ -102,8 +102,6 @@ double DarkZAnnihilation::PreFactor(double E0) {
   //here sigma is in G4 internal units, 1 /Energy^2. Move to pBarn;
   sigma *= GeVtoPb;
 
-  //A.C. correct here for atomic effects
-  sigma = sigma * ZNucl;
   return sigma;
 }
 
