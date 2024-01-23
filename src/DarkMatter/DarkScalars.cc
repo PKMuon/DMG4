e090c6bf0c092fd6c55430644a12d33836a6c6cb
Andrea Celentano <andrea.celentano@cern.ch>
2024-01-23 10:27:50 +0100

    Fixing threshold for A,S,P,V radiative emission

--- src/DarkMatter/DarkScalars.cc
+++ src/DarkMatter/DarkScalars.cc
@@ -55,7 +55,12 @@ double DarkScalars::TotalCrossSectionCalc(double E0)
   //double ThetaMaxEl;
   double sigmaTot;
 
-  if(E0 < 2.*MA) return 0.;
+  if(fabs(ParentPDGID) == 11) MParent = Mel;
+  if(fabs(ParentPDGID) == 13) MParent = Mmu;
+  double Mnucleus=ANucl; //we assume here that Mnucleus = ANucl, not a big change in the formula
+  double EkinMin = MA * (1+MParent/Mnucleus+2*MA/Mnucleus);
+
+  if(E0 < EkinMin ) return 0.;
 
   if(MA > 0.001) { // analytical calculation above 1 MeV
 
