105770eba875fb5b376f1348c2bd881c2a16d560
Andrea Celentano <andrea.celentano@cern.ch>
2024-05-12 18:10:15 +0200

    Update DarkMatterAnnihilation.cc - add check to use "isfinite"

--- src/DarkMatter/DarkMatterAnnihilation.cc
+++ src/DarkMatter/DarkMatterAnnihilation.cc
@@ -203,7 +203,7 @@ double DarkMatterAnnihilation::GetSigmaTotAtomicEffectsOneShellFull(double E0,in
       double zThis=zmin+dz*iz+dz/2;
       double sThis=2*Mel*Mel+2*Ep*(Em-zThis*Pm);
       double preThis=this->PreFactor(sThis)/this->q(sThis);
-      if ((preThis<0)||(preThis!=preThis)){
+      if ((preThis<0)||(!isfinite(preThis))){
         preThis=0; //numerical precision
       }
       pre=pre+preThis;
